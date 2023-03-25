8c26fff54f32f251b2eea5c2ec2dd1373f948cc4
Dariusz Begiedza <4207958@teb-gdansk.ad>
2023-03-25 10:41:23 +0100

    Add files divide

--- index.php
+++ index.php
@@ -8,7 +8,7 @@ include_once
 require
 require_once
 */
-
+include_once('./src/View.php');
 include_once('./src/utils/debug.php');
 
 // dump('test');
@@ -16,44 +16,7 @@ include_once('./src/utils/debug.php');
 
 $action = $_GET['action'] ?? null;
 
+$view = new View();
+$view->render($action);
+
 ?>
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>PHP Notes app</title>
-</head>
-<body>
-    <header>
-        <h1> Moje notatki </h1> 
-    </header>
-    <main>
-        <nav>
-            <ul>
-                <li>
-                    <a href="/">Lista notatek</a>
-                </li>
-                <li>
-                    <a href="/?action=create">Nowa notatka</a>
-                </li>
-            </ul>
-        </nav>
-
-        <article>
-            <?php if ($action === 'create') : ?>
-                <h3>Nowa notatka</h3>
-                <?php echo htmlentities($action) ?>
-            <?php else : ?>
-                <h3>Lista notatek</h3>
-                <?php echo htmlentities($action ?? '') ?>
-            <?php endif; ?>
-        </article>
-        <main>
-
-        <footer>Stopka</footer>
-   
-</body>
-</html>
---
+++ templates/pages/create.php
@@ -0,0 +1,4 @@
+<h3>Nowa notatka</h3>
+<div>
+    tu będzie nowa notatka
+</div>
