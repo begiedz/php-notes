b91f934d5f2bdcb5f09ae924d7b5a7f3c84c8d42
Dariusz Begiedza <4207958@teb-gdansk.ad>
2023-03-26 08:32:40 +0200

    change if to switch

--- index.php
+++ index.php
@@ -14,20 +14,24 @@ $action = $_GET['action'] ?? DEFAULT_ACTION;
 
 $viewParams = [];
 
-if ($action === 'create') {
-    $page = 'create';
-    $created = false;
-    if (!empty($_POST)) {
-        $viewParams = [
-        'title' => $_POST['title'],
-        'description' => $_POST['description'],
-        ];
-        $created = true;
-    }
-    $viewParams['created'] = $created;
-} else {
-    $page = 'list';
 
+switch ($action) {
+    case 'create':
+        $page = 'create';
+        $created = false;
+        if (!empty($_POST)) {
+            $viewParams = [
+            'title' => $_POST['title'],
+            'description' => $_POST['description'],
+            ];
+         $created = true;
+        }
+        $viewParams['created'] = $created;
+        break;
+    default:
+        $page = 'list';
+        $viewParams['resultList'] = 'Wyświetlamy listę notatek';
+        break;
 }
 
 $view = new View();
