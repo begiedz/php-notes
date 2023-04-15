de86fa3a605c1ff8a1664ad9cf7f15fe47b01556
Dariusz Begiedza <begiedz@Dariuszs-MacBook-Air.local>
2023-04-15 09:28:30 +0200

    Add configuration

--- index.php
+++ index.php
@@ -6,7 +6,12 @@ namespace App;
 
 include_once('./src/Controller.php');
 include_once('./src/utils/debug.php');
+require_once('./config/config.php');
 
+
+const DEFAULT_ACTION = 'list';
+
+Controller::initConfiguration($configuration);
 $controller = new Controller($_GET, $_POST);
 $controller->run();
 
