6dc440067adb1c1cef144fc82c4bb330e51589c5
begiedz <dariusz.begiedza@gmail.com>
2023-04-15 11:24:48 +0200

    Add Exception

--- src/Database.php
+++ src/Database.php
@@ -4,18 +4,33 @@ declare(strict_types=1);
 
 namespace App;
 
+use App\Exception\StorageException;
+use App\Exception\Configuration;
 use PDO;
+use PDOException;
+use Throwable;
 
 class Database
 {
     public function __construct(array $config)
     {
-        $dsn = "mysql:dbname={$config['database']};host={$config['host']}";
-        // $connection = new PDO(
-        //     $dsn,
-        //     $config['user'],
-        //     $config['password']
-        // );
-        $connection = new PDO('ssss');
+        try {
+            $this->validateConfig($config);
+            $dsn = "mysql:dbname={$config['database']};host={$config['host']}";
+            $connection = new PDO(
+                $dsn,
+                $config['user'],
+                $config['password']
+            );
+        } catch (PDOException $e) {
+            throw new StorageException('Connection error');
+        }
+    }
+
+    private function validateConfig(array $config): void
+    {
+        if (empty($config['database']) || empty($config['user']) || empty($config['host'])) {
+            throw new ConfigurationException('Problem z konfigurację bazy danych - skontaktuj się z administratorem.');
+        }
     }
 }
